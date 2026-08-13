0d731523c4dd9919eb21034d68f80c1ee815f840
Leila Hajikhani <leilahajikhni18@gmail.com>
2026-08-13 15:59:59 +0330

    update index.html,app.js,input.css

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,14 @@ toggleThemeBtn.addEventListener('click' , function(){
     }
 })
 
+//swiper
 let swiper = new Swiper('.mySwiper', {
     slidesPerView: 1,
     spaceBetween: 22,
-    loop:true,
+    loop: true,
     navigation: {
-        nextEl: '.swiper-button-next-custom',
-        prevEl: '.swiper-button-prev-custom',
+        nextEl: ".swiper-button-next-custom",
+        prevEl: ".swiper-button-prev-custom",
     },
     breakpoints: {
         1024: {
@@ -29,4 +30,62 @@ let swiper = new Swiper('.mySwiper', {
         },
     }
 
+})
+//swiperReading
+let swiperReading = new Swiper('.swiperReading', {
+    slidesPerView: 2,
+    spaceBetween: 8,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next-custom-readings',
+        prevEl: '.swiper-button-prev-custom-readings',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 12,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+        },
+        1536: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    }
+
+})
+
+//mobile sub menu
+let openSubMenuBtn = document.querySelector('.open-sub-menu-btn');
+let mobileSubMenu= document.querySelector('.mobile-sub-menu');
+
+openSubMenuBtn.addEventListener('click' , function(){
+    mobileSubMenu.classList.toggle('hidden')
+    openSubMenuBtn.classList.toggle('rotate-180')
+})
+
+// open and close moblie nav
+let burgerByn=document.querySelector('.burger-btn');
+let mobileNav=document.querySelector('.mobile-nav');
+let blackLayer=document.querySelector('.black-layer');
+let closeBtn=document.querySelector('.close-btn');
+
+burgerByn.addEventListener('click', function(){
+    mobileNav.classList.remove('-right-80')
+    mobileNav.classList.add('right-0')
+    blackLayer.classList.toggle('hidden')
+})
+
+closeBtn.addEventListener('click' , function(){
+    mobileNav.classList.add('-right-80')
+    mobileNav.classList.remove('right-0')
+    blackLayer.classList.toggle('hidden')
+})
+
+blackLayer.addEventListener('click' , function(){
+    mobileNav.classList.add('-right-80')
+    mobileNav.classList.remove('right-0')
+    blackLayer.classList.toggle('hidden')
 })
