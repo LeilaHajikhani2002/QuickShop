53cd2e08aee2e9fc1a3a37dc2be528b1b432972b
Leila Hajikhani <leilahajikhni18@gmail.com>
2026-08-14 19:58:51 +0330

    update index.html,app.js

--- public/js/app.js
+++ public/js/app.js
@@ -111,11 +111,11 @@ blackLayer.addEventListener('click' , function(){
 shoppingCardBtn.addEventListener('click' , function(){
 
     if(window.matchMedia("(max-width: 1279px)").matches){
-        mobileShoppingCard.classList.remove('-bottom-full')
-        mobileShoppingCard.classList.add('bottom-0')
 
+        mobileShoppingCard.classList.toggle('bottom-0')
         blackLayer.classList.toggle('hidden')
     }
+
 
 })
 
